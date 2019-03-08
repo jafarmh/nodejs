ee4a289c574c1e5ebc8003b5cea15a3653edb762
Jafar Mahmoodian <jafar.m@yahoo.com>
2019-03-08 15:43:07 +0330

    validation and toastr js

---
+++ views/toastr.js
@@ -0,0 +1,22 @@
+
+<script>
+function toasterOptions() {
+ toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "100",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "show",
+    "hideMethod": "hide"
+      };
+    }
+</script>
---
+++ views/validation.js
@@ -0,0 +1,27 @@
+<script>
+    function check_other(name){
+        var dd=$("."+name);
+        if (dd.length===0)
+            return false;
+        for(i=0;i<dd.length;i++){
+            if(dd[i].value==='')
+            {
+
+                dd[i].style.boxShadow = "red 0 0 1rem";
+                return true;
+            }else
+            {
+
+                dd[i].style.boxShadow = "";
+            }
+
+        }
+
+        return false;
+
+    }
+    function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+</script>
