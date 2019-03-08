36a9bf0382b491560ae4a3ccfe58cb410542032b
Jafar Mahmoodian <jafar.m@yahoo.com>
2019-03-08 15:43:50 +0330

    check user if exist dont insert

--- Router/register.js
+++ Router/register.js
@@ -14,7 +14,32 @@ Register.post("/register/user/add",(req,res) =>{
   const  myobj = { name: req.body.name, email: req.body.email,password:req.body.Password };
 
 
-  MongoClient.connect(mongourl, function(err, db) {
+MongoClient.connect(mongourl,{ useNewUrlParser: true })
+.then((db) => {
+  var dbo = db.db("mydb");
+
+  dbo.collection('users').findOne({'email': myobj.email})
+  .then((response)=>{
+    if(!response)
+      {
+        dbo.collection("users").insertOne(myobj,function (err,res) {
+          if (err) throw err;
+
+          console.log("1 document inserted");
+          db.close();
+        });
+        res.status("200").send("user add success! now go to login");
+      }
+      else {
+        res.status("500").send("email already exists!");
+      }
+  });
+})
+.catch((err)=>{
+  console.log(err);
+})
+
+  /*MongoClient.connect(mongourl, function(err, db) {
     if (err) throw err;
 
     var dbo = db.db("mydb");
@@ -26,7 +51,7 @@ Register.post("/register/user/add",(req,res) =>{
       db.close();
     })
     res.status("200").send("user add success!");
-  });
+  });*/
 });
 
 module.exports = Register;
--- views/Register/register.js
+++ views/Register/register.js
@@ -1,4 +1,5 @@
 <script>
+let btn=$("#registers");
 $(document).ready (function () {
   rest_frm();
 })
@@ -8,7 +9,25 @@ function rest_frm(){
 }
 
 function register_frm() {
+  btn.prop("disabled",true);
+  toasterOptions();
   let Forms=$("#register_frm").serializeArray();
+
+  if(!validation()){
+    toastr.error("somting is empty");
+      btn.prop("disabled",false);
+      return false;
+  }
+  if (!validateEmail($("#email").val())) {
+    toastr.error("email is Wrong !");
+      btn.prop("disabled",false);
+      return false;
+  }
+  if ($("#Password_again").val()!==$("#Password").val()) {
+    toastr.error("Password is not  equal !");
+      btn.prop("disabled",false);
+      return false
+  }
   $.ajax({
       url:"/register/user/add",
       type:"post",
@@ -16,13 +35,22 @@ function register_frm() {
       data:Forms,
       success:function(data)
       {
-
-        alert (data);
+        toastr.success(data);
+        btn.prop("disabled",false);
       },error:function(data)
       {
-          alert (data.responseText);
+          toastr.error(data.responseText);
+          btn.prop("disabled",false);
       }
   });
+
+}
+
+function validation(){
+  if (check_other("require")) {
+    return false;
+  }
+  return true;
 }
 
 </script>
